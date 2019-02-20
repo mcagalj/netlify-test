3ab130e705a86808ec4dcb324d22dcf6c7c27080
mcagalj <mario.cagalj@fesb.hr>
2019-02-20 08:33:34 +0100

    Fix the problem with absolute imports with new versions of gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,7 +48,7 @@ exports.createPages = ({ actions, graphql }) => {
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   actions.setWebpackConfig({
     resolve: {
-      modules: [path.resolve(__dirname, "./src")]
+      modules: [path.resolve(__dirname, "src"), "node_modules"]
     }
   });
 };
